89b6b03c1b86db4933365a3fbc11337075620c1b
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2025-10-13 23:20:34 +0300

    App: use a camera

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,10 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
