5d9e18ad9216ecb1e6bb701dce4706c11512cd80
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2025-10-14 00:30:30 +0300

    App: failing attempt at rotating around origin

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+use bevy::dev_tools::picking_debug::{DebugPickingMode, DebugPickingPlugin};
 use bevy::prelude::*;
+use std::f32::consts::PI;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(bevy::log::LogPlugin {
+            filter: "bevy_dev_tools=trace".into(),
+            ..default()
+        }))
+        .add_plugins((MeshPickingPlugin, DebugPickingPlugin))
+        .insert_resource(DebugPickingMode::Normal)
         .add_systems(Startup, setup)
         .run();
 }
 
 const X_EXTENT: f32 = 900.;
+const Y_EXTENT: f32 = 900.;
 
 fn setup(
     mut commands: Commands,
@@ -16,7 +24,9 @@ fn setup(
 ) {
     commands.spawn(Camera2d);
 
-    let rough_size = X_EXTENT / 10.;
+    let size = Vec2::new(X_EXTENT, Y_EXTENT);
+    let rough_size = (X_EXTENT + Y_EXTENT) / 2. / 10.;
+    let center = size / 2.;
 
     let shapes = [
         meshes.add(Rectangle::from_length(rough_size)),
@@ -29,17 +39,25 @@ fn setup(
     ];
     let num_shapes = shapes.len();
 
+    let center_indicator = meshes.add(Circle::new(4.));
+
+    commands.spawn((
+        Mesh2d(center_indicator),
+        MeshMaterial2d(materials.add(Color::hsl(1., 1., 1.))),
+    ));
+
     for (i, shape) in shapes.into_iter().enumerate() {
         let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
+        // translate along x axis and then rotate around origin
+        let mut transform = Transform::from_translation(Vec3::X * size.x / 2.)
+            .with_rotation(Quat::from_rotation_z(PI / 2.));
+        transform.translation += transform.forward() * 100.;
+        // let transform = Transform::from_rotation(Quat::from_rotation_z(PI / 2.))
+        //     .with_translation(Vec3::X * size.x / 2.);
         commands.spawn((
             Mesh2d(shape),
             MeshMaterial2d(materials.add(color)),
-            Transform::from_xyz(
-                // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
-                -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
-                0.0,
-                0.0,
-            ),
+            transform,
         ));
     }
 }
