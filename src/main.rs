5094b244cc38092cb369e1520b9f9a81675fbbb8
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2025-10-14 20:15:18 +0300

    Spawn boubas (#3)

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,15 @@ struct Meshes {
 #[derive(Resource)]
 struct State {
     mob_spawn_timer: Timer,
+    bouba_spawn_timer: Timer,
+    mob_scale: f32,
+    bouba_scale: f32,
 }
 
 #[derive(Resource)]
 struct Colors {
     red: Handle<ColorMaterial>,
+    blue: Handle<ColorMaterial>,
 }
 
 #[derive(Resource)]
@@ -28,6 +32,7 @@ struct Arena(Rectangle);
 
 struct Settings {
     initial_mob_spawn_timer_interval: f32,
+    initial_bouba_spawn_timer_interval: f32,
 }
 
 fn main() {
@@ -37,6 +42,7 @@ fn main() {
     }
     let settings = Settings {
         initial_mob_spawn_timer_interval: 0.5,
+        initial_bouba_spawn_timer_interval: 1.0,
     };
     App::new()
         .add_plugins(DefaultPlugins)
@@ -47,9 +53,15 @@ fn main() {
                 settings.initial_mob_spawn_timer_interval,
                 TimerMode::Repeating,
             ),
+            bouba_spawn_timer: Timer::from_seconds(
+                settings.initial_bouba_spawn_timer_interval,
+                TimerMode::Repeating,
+            ),
+            mob_scale: 1.,
+            bouba_scale: 2.,
         })
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, spawn_mobs)
+        .add_systems(FixedUpdate, (spawn_mobs, spawn_boubas))
         .run();
 }
 
@@ -68,7 +80,29 @@ fn spawn_mobs(
         commands.spawn((
             Mesh2d(spawn_meshes.circle.clone()),
             MeshMaterial2d(spawn_colors.red.clone()),
-            Transform::from_translation(Vec3::new(pos.x, pos.y, 0.0)),
+            Transform::from_translation(Vec3::new(pos.x, pos.y, 0.0))
+                .with_scale(Vec3::splat(state.mob_scale)),
+        ));
+    }
+}
+
+fn spawn_boubas(
+    mut commands: Commands,
+    spawn_meshes: Res<Meshes>,
+    spawn_colors: Res<Colors>,
+    mut rand_source: ResMut<RandomSource>,
+    arena: Res<Arena>,
+    mut state: ResMut<State>,
+    time: Res<Time>,
+) {
+    if state.bouba_spawn_timer.tick(time.delta()).just_finished() {
+        let rng = &mut rand_source.0;
+        let pos = arena.0.sample_interior(rng);
+        commands.spawn((
+            Mesh2d(spawn_meshes.circle.clone()),
+            MeshMaterial2d(spawn_colors.blue.clone()),
+            Transform::from_translation(Vec3::new(pos.x, pos.y, 0.0))
+                .with_scale(Vec3::splat(state.bouba_scale)),
         ));
     }
 }
@@ -87,6 +121,7 @@ fn setup(
     };
     let spawn_colors = Colors {
         red: materials.add(Color::hsl(0., 0.92, 0.61)),
+        blue: materials.add(Color::hsl(240., 0.92, 0.61)),
     };
     let arena = Arena(Rectangle::new(800., 600.));
     commands.insert_resource(spawn_meshes);
