088c5e404ce4ef232b26de2c87d3f8668e5fea4e
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2025-10-14 19:40:02 +0300

    Spawn a new mob every 0.5 seconds

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::dev_tools::picking_debug::DebugPickingMode;
+use bevy::math::prelude::*;
 use bevy::prelude::*;
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
@@ -10,6 +11,11 @@ struct Meshes {
 }
 
 #[derive(Resource)]
+struct State {
+    mob_spawn_timer: Timer,
+}
+
+#[derive(Resource)]
 struct Colors {
     red: Handle<ColorMaterial>,
 }
@@ -17,26 +23,54 @@ struct Colors {
 #[derive(Resource)]
 struct RandomSource(ChaCha8Rng);
 
+#[derive(Resource)]
+struct Arena(Rectangle);
+
+struct Settings {
+    initial_mob_spawn_timer_interval: f32,
+}
+
 fn main() {
     #[cfg(target_os = "macos")]
     unsafe {
         env::set_var("MTL_HUD_ENABLED", "1");
     }
+    let settings = Settings {
+        initial_mob_spawn_timer_interval: 0.5,
+    };
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(MeshPickingPlugin)
         .insert_resource(DebugPickingMode::Normal)
+        .insert_resource(State {
+            mob_spawn_timer: Timer::from_seconds(
+                settings.initial_mob_spawn_timer_interval,
+                TimerMode::Repeating,
+            ),
+        })
         .add_systems(Startup, setup)
         .add_systems(FixedUpdate, spawn_mobs)
         .run();
 }
 
-fn spawn_mobs(mut commands: Commands, spawn_meshes: Res<Meshes>, spawn_colors: Res<Colors>) {
-    commands.spawn((
-        Mesh2d(spawn_meshes.circle.clone()),
-        MeshMaterial2d(spawn_colors.red.clone()),
-        Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
-    ));
+fn spawn_mobs(
+    mut commands: Commands,
+    spawn_meshes: Res<Meshes>,
+    spawn_colors: Res<Colors>,
+    mut rand_source: ResMut<RandomSource>,
+    arena: Res<Arena>,
+    mut state: ResMut<State>,
+    time: Res<Time>,
+) {
+    if state.mob_spawn_timer.tick(time.delta()).just_finished() {
+        let rng = &mut rand_source.0;
+        let pos = arena.0.sample_interior(rng);
+        commands.spawn((
+            Mesh2d(spawn_meshes.circle.clone()),
+            MeshMaterial2d(spawn_colors.red.clone()),
+            Transform::from_translation(Vec3::new(pos.x, pos.y, 0.0)),
+        ));
+    }
 }
 
 fn setup(
@@ -44,9 +78,9 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    // resources
     let seeded_rng = ChaCha8Rng::seed_from_u64(19878367467712);
     commands.insert_resource(RandomSource(seeded_rng));
-    commands.spawn(Camera2d);
     let mob_size = 16.;
     let spawn_meshes = Meshes {
         circle: meshes.add(Circle::new(mob_size)),
@@ -54,7 +88,10 @@ fn setup(
     let spawn_colors = Colors {
         red: materials.add(Color::hsl(0., 0.92, 0.61)),
     };
+    let arena = Arena(Rectangle::new(800., 600.));
     commands.insert_resource(spawn_meshes);
     commands.insert_resource(spawn_colors);
-    // let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
+    commands.insert_resource(arena);
+    // entities
+    commands.spawn(Camera2d);
 }
