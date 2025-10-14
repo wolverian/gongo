a34dec5d1a9c5fe1b8d203f9710a0ac66c58354f
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2025-10-14 19:06:39 +0300

    Add randomness and MTL debug overlay

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,60 @@
-use bevy::dev_tools::picking_debug::{DebugPickingMode, DebugPickingPlugin};
+use bevy::dev_tools::picking_debug::DebugPickingMode;
 use bevy::prelude::*;
-use std::f32::consts::PI;
+use rand::SeedableRng;
+use rand_chacha::ChaCha8Rng;
+use std::env;
+
+#[derive(Resource)]
+struct Meshes {
+    circle: Handle<Mesh>,
+}
+
+#[derive(Resource)]
+struct Colors {
+    red: Handle<ColorMaterial>,
+}
+
+#[derive(Resource)]
+struct RandomSource(ChaCha8Rng);
 
 fn main() {
+    #[cfg(target_os = "macos")]
+    unsafe {
+        env::set_var("MTL_HUD_ENABLED", "1");
+    }
     App::new()
-        .add_plugins(DefaultPlugins.set(bevy::log::LogPlugin {
-            filter: "bevy_dev_tools=trace".into(),
-            ..default()
-        }))
-        .add_plugins((MeshPickingPlugin, DebugPickingPlugin))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(MeshPickingPlugin)
         .insert_resource(DebugPickingMode::Normal)
         .add_systems(Startup, setup)
+        .add_systems(FixedUpdate, spawn_mobs)
         .run();
 }
 
-const X_EXTENT: f32 = 900.;
-const Y_EXTENT: f32 = 900.;
+fn spawn_mobs(mut commands: Commands, spawn_meshes: Res<Meshes>, spawn_colors: Res<Colors>) {
+    commands.spawn((
+        Mesh2d(spawn_meshes.circle.clone()),
+        MeshMaterial2d(spawn_colors.red.clone()),
+        Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+    ));
+}
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    let seeded_rng = ChaCha8Rng::seed_from_u64(19878367467712);
+    commands.insert_resource(RandomSource(seeded_rng));
     commands.spawn(Camera2d);
-
-    let size = Vec2::new(X_EXTENT, Y_EXTENT);
-    let rough_size = (X_EXTENT + Y_EXTENT) / 2. / 10.;
-    let center = size / 2.;
-
-    let shapes = [
-        meshes.add(Rectangle::from_length(rough_size)),
-        meshes.add(Circle::new(rough_size)),
-        meshes.add(Triangle2d::new(
-            Vec2::from_angle(0.) * rough_size,
-            Vec2::from_angle(90.0) * rough_size,
-            Vec2::from_angle(180.0) * rough_size,
-        )),
-    ];
-    let num_shapes = shapes.len();
-
-    let center_indicator = meshes.add(Circle::new(4.));
-
-    commands.spawn((
-        Mesh2d(center_indicator),
-        MeshMaterial2d(materials.add(Color::hsl(1., 1., 1.))),
-    ));
-
-    for (i, shape) in shapes.into_iter().enumerate() {
-        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
-        // translate along x axis and then rotate around origin
-        let mut transform = Transform::from_translation(Vec3::X * size.x / 2.)
-            .with_rotation(Quat::from_rotation_z(PI / 2.));
-        transform.translation += transform.forward() * 100.;
-        // let transform = Transform::from_rotation(Quat::from_rotation_z(PI / 2.))
-        //     .with_translation(Vec3::X * size.x / 2.);
-        commands.spawn((
-            Mesh2d(shape),
-            MeshMaterial2d(materials.add(color)),
-            transform,
-        ));
-    }
+    let mob_size = 16.;
+    let spawn_meshes = Meshes {
+        circle: meshes.add(Circle::new(mob_size)),
+    };
+    let spawn_colors = Colors {
+        red: materials.add(Color::hsl(0., 0.92, 0.61)),
+    };
+    commands.insert_resource(spawn_meshes);
+    commands.insert_resource(spawn_colors);
+    // let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
 }
