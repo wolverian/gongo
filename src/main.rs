f2868c54c8e63f5af74abe71b3f1093feabb5968
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2025-10-16 18:34:40 +0300

    App: add collisions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use avian2d::prelude::*;
 use bevy::math::prelude::*;
 use bevy::prelude::*;
-use rand::SeedableRng;
+use rand::distr::Uniform;
+use rand::{Rng, SeedableRng};
 use rand_chacha::ChaCha8Rng;
 #[cfg(target_os = "macos")]
 use std::env;
@@ -31,6 +32,9 @@ struct RandomSource(ChaCha8Rng);
 #[derive(Resource)]
 struct Arena(Rectangle);
 
+#[derive(Resource)]
+struct MobVelocityDistribution(Uniform<f32>);
+
 struct Settings {
     initial_mob_spawn_timer_interval: f32,
     initial_bouba_spawn_timer_interval: f32,
@@ -48,8 +52,9 @@ fn main() {
     App::new()
         // Plugins
         .add_plugins(DefaultPlugins)
-        .add_plugins(PhysicsPlugins::default())
+        .add_plugins(PhysicsPlugins::default().with_length_unit(20.)) // 1 meter = 20 pixels
         // Resources
+        .insert_resource(Gravity::ZERO)
         .insert_resource(RandomSource(ChaCha8Rng::seed_from_u64(42)))
         .insert_resource(Arena(Rectangle::new(800., 600.)))
         .insert_resource(State {
@@ -64,6 +69,9 @@ fn main() {
             mob_scale: 1.,
             bouba_scale: 2.,
         })
+        .insert_resource(MobVelocityDistribution(
+            Uniform::new(-32., 32.).expect("Failed to create uniform distribution"),
+        ))
         // Debug plugins
         .add_plugins(MeshPickingPlugin)
         .add_plugins((
@@ -85,18 +93,32 @@ fn spawn_mobs(
     arena: Res<Arena>,
     mut state: ResMut<State>,
     time: Res<Time>,
+    mob_velocity_distribution: Res<MobVelocityDistribution>,
 ) {
     if !state.mob_spawn_timer.tick(time.delta()).just_finished() {
         return;
     }
 
+    let mob_size = 16.; // todo: dry this out
+    let circle = Circle::new(mob_size);
+
     let rng = &mut rand_source.0;
     let pos = arena.0.sample_interior(rng);
+    let distr = Uniform::new(-10., 10.).expect("Failed to create uniform distribution");
+    let mut collider = circle.collider();
+    collider.set_scale(Vec2::splat(state.mob_scale), 32);
     commands.spawn((
         Mesh2d(spawn_meshes.circle.clone()),
         MeshMaterial2d(spawn_colors.red.clone()),
+        collider,
+        RigidBody::Dynamic,
         Transform::from_translation(Vec3::new(pos.x, pos.y, 0.0))
             .with_scale(Vec3::splat(state.mob_scale)),
+        LinearVelocity(Vec2::new(
+            rng.sample(mob_velocity_distribution.0),
+            rng.sample(mob_velocity_distribution.0),
+        )),
+        Restitution::new(1.),
     ));
 }
 
@@ -115,11 +137,18 @@ fn spawn_boubas(
 
     let rng = &mut rand_source.0;
     let pos = arena.0.sample_interior(rng);
+    let distr = Uniform::new(-10., 10.).expect("Failed to create uniform distribution");
+    let mut collider = Circle::new(16.).collider();
+    collider.set_scale(Vec2::splat(state.bouba_scale), 32);
     commands.spawn((
         Mesh2d(spawn_meshes.circle.clone()),
         MeshMaterial2d(spawn_colors.blue.clone()),
+        collider,
+        RigidBody::Dynamic,
         Transform::from_translation(Vec3::new(pos.x, pos.y, 0.0))
             .with_scale(Vec3::splat(state.bouba_scale)),
+        LinearVelocity(Vec2::new(rng.sample(distr), rng.sample(distr))),
+        Restitution::new(1.),
     ));
 }
 
